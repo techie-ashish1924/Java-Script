82b2d26864512ba28e056742ad7cf4160584f2d4
Ashish Sengar <techie.sengar1924@gmail.com>
2022-10-13 09:37:24 +0530

    final commit

---
+++ Arrays/main.js
@@ -0,0 +1,15 @@
+
+// Arrays
+
+let arr = new Array(5);
+
+console.log(arr);
+
+// We can input multiple type data in array in java script
+
+let arr2 = [1,2,3,4,5,6,"Ashish",true,function(){alert ('hello world')}];
+
+console.log(arr2);
+
+console.log("element at index 2 is : ", arr2[2]);
+console.log(arr2[8]())
