adc4035078f2da790fc93868dc8fe01d5b3a514c
Ashish Sengar <techie.sengar1924@gmail.com>
2022-10-19 09:49:52 +0530

    final commit

---
+++ Objects/main.js
@@ -0,0 +1,65 @@
+// objects
+// 
+// let obj = {
+// 
+    // "hello1" : 1,
+    // "hello2" : 2,
+// 
+    // Welcome : function()
+    // {
+        // console.log('welcome')
+    // },
+        // arr:['maruti','swift','baleno'],
+        // 'new car' : 'audi',
+    // 
+// }
+// 
+// console.log(obj.name);
+// 
+// console.log(obj["hello1"]);
+// 
+// objext values 
+// 
+// console.log(Object.keys(obj));
+// 
+// object values
+// 
+// console.log(Object.values(obj));
+// 
+// console.log(Object.values(obj));
+
+
+// print the occurance of letters in a s tring in key : value form
+
+let str = "aabc bbca caab ddac";
+
+// let arr = new Array(26);
+
+let obj = {};
+
+for(let i of str)
+{
+    if(!obj[i])
+    {
+        obj[i] = 1;
+    }
+    else{
+        obj[i] = obj[i] + 1;
+    }
+}
+
+console.log(obj);
+
+// for(let i=0;i<str.length;i++)
+// {
+    // arr[str.charAt(i) - 97]++;
+// 
+// }
+
+// for(let i=0;i<arr.length;i++)
+// {
+    // console.log(arr[i]);
+// }
+
+
+// console.log(arr);
