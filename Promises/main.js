ff5ceb05d0188cbd78cb3562b7170127512fce6b
Ashish Sengar <techie.sengar1924@gmail.com>
2022-11-03 10:19:48 +0530

    final commit

--- Promises/main.js
+++ Promises/main.js
@@ -1,12 +1,18 @@
-// promises -> states 
+/* promises -> states 
 
-// 1 ->  resolve state - work has done.
-// 2 -> reject  -> work has not done.
-// 3 -> pending state -> nor reject or resolve.
+     1 ->  resolve state - work has done.
+     2 -> reject  -> work has not done.
+     3 -> pending state -> nor reject or resolve.
+
+    - multiple callbacks
+    - anytime and any where
+ 
+
+*/
 
 // resolve function
 
-const p = new Promise(function(resolve,reject)
+const p = new Promise((function(resolve,reject)
 {
     setTimeout(()=>
     {
@@ -17,8 +23,24 @@ const p = new Promise(function(resolve,reject)
             resolve(randomNum);
         }
         else{
-            reject();
+            reject(randomNum);
         }
-    })
+    },2000)
 
+}))
+
+p.then((data) =>
+{
+    console.log(data);
+    console.log("resolve");
+    return "abc";
+
+}).then((data) =>
+{
+    console.log(data);
+    console.log("resolve");
+}).catch((data)=>
+{
+    console.log(data);
+    console.log("reject");
 })
