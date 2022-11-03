480b458d49d0faea9247745aa195c10fd9bc1262
Ashish Sengar <techie.sengar1924@gmail.com>
2022-11-03 11:19:21 +0530

    final commit

---
+++ Asynchronous Await/main.js
@@ -0,0 +1,19 @@
+
+async function abc()
+{
+    return new Promise((resolve,reject) =>
+    {
+        resolve(data);
+        console.log("hello");
+    })
+}
+
+
+function abc()
+{
+    const p = new Promise((resolve,reject) =>
+    {
+        resolve(data);
+        console.log("hello");
+    })
+}
