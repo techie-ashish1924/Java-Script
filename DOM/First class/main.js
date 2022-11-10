292900ee341c17c66ca91e3e0af0ac0dce1cf460
Ashish Sengar <techie.sengar1924@gmail.com>
2022-11-10 10:56:25 +0530

    final commit

--- DOM/First class/main.js
+++ DOM/First class/main.js
@@ -2,3 +2,21 @@
 console.dir(document);
 
 
+console.log(document.domain);
+
+console.log(document.URL);
+
+console.log(document.body);
+
+console.log(document.head);
+
+console.log(document.links);
+
+console.log(document.all);
+
+// document.all[5].textContent = 'hello';
+
+
+let header = document.getElementById('header-title');
+
+header.style.backgroundColor = 'green';                                                  
